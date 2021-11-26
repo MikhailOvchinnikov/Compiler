4402aed0222014b237a4ae75651f9e10b120bc3d
Mikhail <78845298+MikhailOvchinnikov@users.noreply.github.com>
2021-11-26 20:15:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Compiler.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "compiler.h"
+#include <iostream>
+
+int main()
+{
+    CompilingFile();
+}
+
